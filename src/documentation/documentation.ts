a538896db03d8d1a15b6b2e12211139eefc8757b
Doug Kent <github@douglaskent.com>
2021-03-26 05:19:44 -0600

    change documentation link

--- src/documentation/documentation.ts
+++ src/documentation/documentation.ts
@@ -1,7 +1,7 @@
 export class Documentation {
 
   canActivate(): boolean {
-    window.open("https://docs.primedao.io/", "_blank");
+    window.open("https://docs.primedao.io/primedao/products/poolmanager", "_blank");
     return false;
   }
 }
