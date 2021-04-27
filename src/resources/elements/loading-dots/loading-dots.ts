fc7aa3bc88b7761927894777ed4a4295657d1f69
Doug Kent <github@douglaskent.com>
2021-04-27 14:22:46 -0600

    add tooltip to loading dots

--- src/resources/elements/loading-dots/loading-dots.ts
+++ src/resources/elements/loading-dots/loading-dots.ts
@@ -1,3 +1,6 @@
+import { bindable } from "aurelia-typed-observable-plugin";
 import "./loading-dots.scss";
 
-export class LoadingDots {}
+export class LoadingDots {
+  @bindable.string tooltip?: string;
+}
